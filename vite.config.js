5a6e60d1066844af4fb16a17fa997ae6b4ce3055
ishuki916 <89925767+ishuki916@users.noreply.github.com>
2024-09-06 13:19:02 +0800

    revise case url

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,8 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  base: '/vite-to-do-list/',
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
