88f6ddc2273fde362b1f609170bb58bcb9da9c2d
ishuki916 <89925767+ishuki916@users.noreply.github.com>
2024-09-07 17:14:46 +0800

    complete

--- src/views/ToDoListView.vue
+++ src/views/ToDoListView.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, onMounted, computed } from 'vue'
+import { ref, onMounted, watch } from 'vue'
 import { useRouter } from 'vue-router'
 import axios from 'axios'
 import { walk } from 'vue/compiler-sfc'
@@ -10,8 +10,11 @@ const userName = ref('')
 const myCookie = ref('')
 const isValid = ref(false)
 const toDoList = ref({})
-const toDoSize = computed(() => toDoList.value.length)
+const toDoSize = ref(0)
 const content = ref('')
+const activeIndex = ref(0)
+const unFinishedList = ref([])
+const finishedList = ref([])
 onMounted(async () => {
   try {
     myCookie.value = document.cookie.replace(/(?:(?:^|.*;\s*)tkn\s*\=\s*([^;]*).*$)|^.*$/, '$1')
@@ -30,6 +33,11 @@ onMounted(async () => {
       })
 
       toDoList.value = resTodo.data.data
+      toDoList.value.forEach((item) => {
+        if (!item.status) {
+          toDoSize.value++
+        }
+      })
     }
   } catch (err) {
     loginPage()
@@ -76,6 +84,8 @@ const insertTodo = async () => {
       }
     )
     toDoList.value.push(res.data.newTodo)
+    toDoSize.value++
+    unFinishedList.value.push(res.data.newTodo)
   } catch (err) {
     console.log('insert err:', err.response.data.message)
   } finally {
@@ -84,6 +94,12 @@ const insertTodo = async () => {
 }
 const toggleToDo = async (toDO) => {
   try {
+    toDO.status = !toDO.status
+    if (toDO.status) {
+      toDoSize.value--
+    } else {
+      toDoSize.value++
+    }
     const res = await axios.patch(
       `https://todolist-api.hexschool.io/todos/${toDO.id}/toggle`,
       {},
@@ -100,11 +116,15 @@ const toggleToDo = async (toDO) => {
 
 const deleteToDo = async (toDO) => {
   try {
+    if (!toDO.status) {
+      toDoSize.value--
+    }
     const res = await axios.delete(`https://todolist-api.hexschool.io/todos/${toDO.id}`, {
       headers: {
         authorization: myCookie.value
       }
     })
+
     toDoList.value.forEach((item, index) => {
       if (item.id === toDO.id) {
         toDoList.value.splice(index, 1)
@@ -113,6 +133,30 @@ const deleteToDo = async (toDO) => {
   } catch (err) {
     console.log('delete err:', err.response.data.message)
   }
+}
+
+const setActiveIndex = (index) => {
+  activeIndex.value = index
+  finishedList.value = []
+  unFinishedList.value = []
+}
+
+const handleUnFinished = () => {
+  setActiveIndex(1)
+  toDoList.value.forEach((item) => {
+    if (!item.status) {
+      unFinishedList.value.push(item)
+    }
+  })
+}
+
+const handleFinished = () => {
+  setActiveIndex(2)
+  toDoList.value.forEach((item) => {
+    if (item.status) {
+      finishedList.value.push(item)
+    }
+  })
 }
 </script>
 <template>
@@ -136,24 +180,36 @@ const deleteToDo = async (toDO) => {
         </div>
         <div class="todoList_list">
           <ul class="todoList_tab">
-            <li><a id="allTag" class="active">全部</a></li>
-            <li><a id="toToTag">待完成</a></li>
-            <li><a id="finishedTag">已完成</a></li>
+            <li><a @click="setActiveIndex(0)" :class="{ active: activeIndex === 0 }">全部</a></li>
+            <li><a @click="handleUnFinished" :class="{ active: activeIndex === 1 }">待完成</a></li>
+            <li><a @click="handleFinished" :class="{ active: activeIndex === 2 }">已完成</a></li>
           </ul>
           <div class="todoList_items">
             <ul class="todoList_item">
-              <li v-if="toDoSize === 0">目前尚無待辦事項</li>
-              <AllToDoView
-                v-for="toDo in toDoList"
-                :key="toDo.id"
-                :todo="toDo"
-                @delete="deleteToDo(toDo)"
-                @toggle="toggleToDo(toDo)"
-              />
+              <li v-if="toDoList.length === 0">目前尚無待辦事項</li>
+              <div v-if="activeIndex === 0">
+                <AllToDoView
+                  v-for="toDo in toDoList"
+                  :key="toDo.id"
+                  :todo="toDo"
+                  @delete="deleteToDo(toDo)"
+                  @toggle="toggleToDo(toDo)"
+                />
+                <div class="todoList_statistics">
+                  <p>{{ toDoSize }} 個待完成項目</p>
+                </div>
+              </div>
+              <div v-if="activeIndex === 1">
+                <li v-for="unf in unFinishedList" :key="unf.id">
+                  <span>{{ unf.content }}</span>
+                </li>
+              </div>
+              <div v-if="activeIndex === 2">
+                <li v-for="fi in finishedList" :key="fi.id">
+                  <span>{{ fi.content }}</span>
+                </li>
+              </div>
             </ul>
-            <div class="todoList_statistics">
-              <p>{{ toDoSize }} 個待完成項目</p>
-            </div>
           </div>
         </div>
       </div>
